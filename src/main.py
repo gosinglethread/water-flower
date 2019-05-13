adb2232aaf3f84fbe25660ba99cf2970bb2d445c
liubing <singlethread@aliyun.com>
2019-05-13 16:27:36 +0800

    class

--- src/main.py
+++ src/main.py
@@ -7,25 +7,54 @@ pump_control_pin = Pin(11, Pin.OUT)
 water_sensor_pin = Pin(10)
 
 class Conf(object):
-    def __init__(self, *args):
-        super(Conf, self).__init__(*args))
+    def __init__(self, remote_addr=''):
         self.__check_water_interval = 1
         self.__water_low_num = 100
     
-    def get_water_low_num():
+    def get_water_low_num(self):
         return self.__water_low_num
 
-    def get_check_water_interval():
+    def get_check_water_interval(self):
         return self.__check_water_interval
+    
+    def update_from_remote(self):
+        pass
 
+    def start_update_interval(self):
+        pass
+		
 
-global_conf = Conf()
+class Pumper(object):
+    def __init__(self, pin_num, on_timer_id=57):
+        self.__pin = Pin(pin_num, Pin.OUT)
+        self.__time_to_on = 0
+        self.__on_timer_id = on_timer_id
+        self.__on_timer = None
 
-def get_conf():
-    return global_conf
-
-def get_remote_conf():
-    return Conf()
+    def is_on(self):
+        return self.__time_to_on > 0
+    
+    def on(self):
+        print('pumper: on')
+        self.__pin.on()
+    
+    def off(self):
+        print('pumper: off')
+        self.__pin.off()
+        self.__time_to_on = 0
+        self.__on_timer.deinit()
+        self.__on_timer = None
+    
+    def on_for_milliseconds(self, milliseconds=1000):
+        if self.__time_to_on > milliseconds:
+            print('pumper: already on until ', self.__time_to_on)
+            return
+        self.__time_to_on = milliseconds
+        print('pumper: delay on until ', self.__time_to_on)
+        self.on()
+        if self.__on_timer is not None:
+            self.__on_timer.deinit()
+        self.__on_timer = Timer(self.__on_timer_id).init(period=self.__time_to_on, mode=Timer.ONE_SHOT, callback=self.off)
 
 
 
@@ -44,22 +73,19 @@ def power_pump():
 
 def power_pump_for_milliseconds(milliseconds=1000):
     power_pump()
-    Timer(-1).init(period=milliseconds, Timer.ONE_SHOT, dis_power_pump)
+
 
 def dis_power_pump():
     pump_control_pin.off()
 
 def check_water():
-    cfg = get_conf()
+    cfg = Conf()
     water_num = get_water_num()
     is_water_low = check_water_low(cfg, water_num)
     if is_water_low:
         handle_water_low()
 
-def update_global_conf():
-    global_conf = get_remote_conf
-
 def main():
-    global_conf = get_remote_conf()
-    Timer(1).init(period=global_conf.get_check_water_interval, mode=Timer.PERIOD, callback=check_water)
-    Timer(2).init(period=global_conf.get_check_water_interval, mode=Timer.PERIOD, callback=update_global_conf)
+    cfg = Conf()
+    Timer(1).init(period=cfg.get_check_water_interval, mode=Timer.PERIOD, callback=check_water)
+
